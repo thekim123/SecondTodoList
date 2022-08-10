32f39159c6d41d9c6096b9ce41cfdf9294d0d550
Younghwan <tacdo12@naver.com>
2022-08-11 00:05:55 +0900

    first commit

---
+++ src/main/java/com/thekim12/main/SecondTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.thekim12.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondTodoListApplication.class, args);
+	}
+
+}
